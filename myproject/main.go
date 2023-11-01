27775fbe60694d141182a0ccd82fe5624c5910fb
Dinesh Kolla <dineshkolla@Dineshs-MacBook-Air.local>
2023-11-01 16:09:44 -0500

    first commit

---
+++ myproject/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"myproject/simplecalc"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(simplecalc.Add(3, 5))
+}
---
+++ myproject/simplecalc/calc.go
@@ -0,0 +1,5 @@
+package simplecalc
+
+func Add(x, y int) int {
+	return x + y
+}
